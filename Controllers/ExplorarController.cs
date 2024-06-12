e643f2447fe40ed878e3a900af418a0f686285f4
Daniel Ovalle <danielalanis1000@gmail.com>
2024-06-11 18:19:44 -0600

    cambios 11-06-2024

---
+++ Controllers/ExplorarController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Twitter.Controllers
+{
+    public class ExplorarController : Controller
+    {
+        public IActionResult Explorar()
+        {
+            return View();
+        }
+    }
+}
